8fefa38b8c277590aee4092512b82dbfada74e4f
Eva <evalowsh@gmail.com>
2024-12-13 13:25:30 +0800

    about

--- src/app/about-me/page.tsx
+++ src/app/about-me/page.tsx
@@ -1,21 +1,33 @@
-import { CardHeader, CardTitle, CardContent, CardDescription } from "@/components/ui/card";
+import { Card, CardHeader, CardTitle, CardContent, CardDescription } from "@/components/ui/card";
 
 
 export default function AboutMe() {
     return (
-        <div>
-            <CardHeader>
-                <CardTitle>
-                    SUPPPPPPP
-                </CardTitle>
-                <CardDescription>
+        <div className="flex justify-around px-8 pt-12">
+            <Card className="w-2/5 h-36">
+                <CardHeader>
+                    <CardTitle>
+                        SUPPPPPPP
+                    </CardTitle>
+                    <CardDescription>
 
-                hiii, i'm eva
-                </CardDescription>
+                    hiii, i'm eva
+                    </CardDescription>
 
-            </CardHeader>
+                </CardHeader>
+            </Card>
+            <Card className="w-2/5 h-36">
+                <CardHeader>
+                    <CardTitle>
+                        HELLLOOOO
+                    </CardTitle>
+                    <CardDescription>
 
+                        i'm damien!
+                    </CardDescription>
 
+                </CardHeader>
+            </Card>
         </div>
 
 
